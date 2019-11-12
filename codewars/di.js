bc4650c85652fc10849768aca5fbf65cde4ec748
Djordje Mancovic <djordje.mancovic@hyperoptic.com>
2019-11-12 14:55:23 +0100

    DI

---
+++ codewars/di.js
@@ -0,0 +1,64 @@
+/**
+ Did you hear about Dependency Injection pattern? The main idea of this pattern is that you may have ability to pass dependencies into your function in any order and they will be resolved automatically. Take a look at this:
+
+ var myDependentFunc = inject(function (secondDepency, firstDependency) {
+  firstDependency();
+  secondDepency();
+});
+
+ myDependentFunc();
+ As you can see we just put some variables into the function's signature and work with them as usualy. But we know nothing about where these variables are located! Let's assume that all dependencies are stored in the single hash object (for instanse 'deps') and the injector function will be sought among 'deps' properties:
+
+ var deps = {
+  'firstDependency': function () {return 'this is firstDependency';},
+  'secondDepency': function () {return 'this is secondDepency';},
+};
+ Ok, I hope this is clear. Also, in order to setup DependencyInjector (DI) we need to specify dependency object:
+
+ var DI = function (dependency) {
+  this.dependency = dependency;
+};
+ Your task is create DI.prototype.inject method that will return a new function with resolved dependencies. And don't forget about nested functions. You shouldn't handle them.
+
+ URL: "https://www.codewars.com/kata/5302d655be2a91068b0001fb/train/javascript"
+ */
+
+/**
+ * Constructor DependencyInjector
+ * @param {Object} - object with dependencies
+ */
+var DI = function (dependency) {
+    this.dependency = dependency;
+};
+
+// Should return new function with resolved dependencies
+DI.prototype.inject = function (func) {
+
+    // TODO try top get arguments.
+    console.log(func.toString())
+
+    let dependency = this.dependency;
+
+    return function() {
+        return func(dependency.dep3, dependency.dep1, dependency.dep2);
+    };
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+var deps = {
+    'dep1': function () {return 'this is dep1';},
+    'dep2': function () {return 'this is dep2';},
+    'dep3': function () {return 'this is dep3';},
+    'dep4': function () {return 'this is dep4';}
+};
+
+var di = new DI(deps);
+
+var myFunc = di.inject(function (dep3, dep1, dep2) {
+    return [dep1(), dep2(), dep3()].join(' -> ');
+});
+
+console.log(myFunc(), 'this is dep1 -> this is dep2 -> this is dep3');
---
+++ codewars/strip_comments.js
@@ -0,0 +1,40 @@
+/**
+ Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+ Any whitespace at the end of the line should also be stripped out.
+
+ Example:
+
+ Given an input string of:
+
+ apples, pears # and bananas
+ grapes
+ bananas !apples
+ The output expected would be:
+
+ apples, pears
+ grapes
+ bananas
+ The code would be called like so:
+
+ var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+ // result should == "apples, pears\ngrapes\nbananas"
+ */
+
+function solution(input, markers) {
+    var lines = input.split("\n");
+
+    for(let i = 0; i < lines.length; i++) {
+        markers.forEach(function (marker) {
+            if(lines[i].indexOf(marker) !== -1) {
+                lines[i] = (lines[i].substring(0, lines[i].indexOf(marker))).replace(/\s+$/, '');
+            }
+        })
+    }
+
+    return lines.join("\n");
+};
+
+var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"
+
+console.log(result);
